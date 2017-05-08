996ffb1bb977c0cfd51953ef8a7f0561cb09ee60
thavelka <tim.havelka@gmail.com>
2017-05-08 00:26:12 -0500

    Set up adapter in ListingsActivity

--- app/src/main/java/com/thavelka/feedme/ui/ListingsActivity.java
+++ app/src/main/java/com/thavelka/feedme/ui/ListingsActivity.java
@@ -6,17 +6,24 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.thavelka.feedme.R;
+import com.thavelka.feedme.models.Listing;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 
 public class ListingsActivity extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private Realm realm;
+    private RealmResults<Listing> listings;
+    private RealmChangeListener<RealmResults<Listing>> changeListener;
+    private ListingAdapter adapter;
 
     @BindView(R.id.listings_navigation) BottomNavigationView bottomNavigationView;
     @BindView(R.id.listings_recyclerView) RecyclerView recyclerView;
@@ -25,15 +32,26 @@ public class ListingsActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         realm = Realm.getDefaultInstance();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listings);
         ButterKnife.bind(this);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        listings = realm.where(Listing.class).findAll();
+        changeListener = element -> {
+            if (element != null && element.isValid() && element.isLoaded() && adapter != null) {
+                adapter.notifyDataSetChanged();
+            }
+        };
+        listings.addChangeListener(changeListener);
+        adapter = new ListingAdapter(this, listings, listing ->
+                Toast.makeText(this, "Tapped listing", Toast.LENGTH_SHORT).show());
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
     protected void onDestroy() {
-        if (realm != null) realm.close();
+        if (changeListener != null) listings.removeChangeListener(changeListener);
         super.onDestroy();
+        if (realm != null) realm.close();
     }
 
     @Override
