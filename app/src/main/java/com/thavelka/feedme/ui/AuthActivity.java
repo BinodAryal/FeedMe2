4565081c1329e14b39f56225bd2717024db96747
thavelka <tim.havelka@gmail.com>
2017-06-24 16:19:42 -0500

    Add AuthActivity

---
+++ app/src/main/java/com/thavelka/feedme/ui/AuthActivity.java
@@ -0,0 +1,15 @@
+package com.thavelka.feedme.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thavelka.feedme.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
